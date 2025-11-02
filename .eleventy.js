e09318605884a3092b7a23d74a8332a1e9260eae
Sat Purusha <69746648+SatPurusha@users.noreply.github.com>
2025-11-02 08:55:20 +0100

    Tidy up watch targets

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 export default function(config) {
   config.addPassthroughCopy('src/images')
-  config.addWatchTarget("dist/css/style.css")
   config.setServerOptions({
     watch: ["dist/css/style.css"]
   })
