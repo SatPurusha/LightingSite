9330efa5f0fbef3e304066f35bdc5832e74a8279
Sat Purusha <69746648+SatPurusha@users.noreply.github.com>
2025-11-06 16:32:01 +0100

    Clarify edits

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,18 @@
-import imageInspire from "./src/_includes/shortcodes/imageInspire.js";
+import iText from "./src/_includes/shortcodes/imageInspire.js";
 
 export default (eleventyConfig) => {
-  eleventyConfig.addShortcode("imageInspire", imageInspire);
-  eleventyConfig.addPassthroughCopy("src/images")
-    watch: ["dist/css/style.css"];
+  eleventyConfig.addShortcode("scii", iText);
+  eleventyConfig.addPassthroughCopy("src/images");
+  watch: ["dist/css/style.css"];
 };
 
 export const config = {
   markdownTemplateEngine: "njk",
   htmlTemplateEngine: "njk",
   dir: {
-    data: "_data",
     input: "src",
+    data: "_data",
     includes: "_includes",
-    layouts: "_includes/layouts",
     shortcodes: "_includes/shortcodes",
     output: "dist",
     images: "images",
